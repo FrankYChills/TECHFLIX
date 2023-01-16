f54733b30293d6d6cb21d48c74e7776ba52ad807
franky <itskchills11@gmail.com>
2023-01-16 19:56:22 +0530

    hover on display

--- src/components/listitem/ListItem.jsx
+++ src/components/listitem/ListItem.jsx
@@ -2,32 +2,45 @@ import PlayCircleFilledWhiteOutlinedIcon from "@mui/icons-material/PlayCircleFil
 import AddCircleOutlineOutlinedIcon from "@mui/icons-material/AddCircleOutlineOutlined";
 import ThumbUpOutlinedIcon from "@mui/icons-material/ThumbUpOutlined";
 import ThumbDownOutlinedIcon from "@mui/icons-material/ThumbDownOutlined";
+import { useState } from "react";
 
 import "./ListItem.scss";
 
 const ListItem = () => {
+  const trailer = "videos/thumbnail.mp4";
+
+  const [isHovered, setIsHovered] = useState(false);
   return (
-    <div className="listItem">
+    <div
+      className="listItem"
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
       <img src="images/list-image.jpg" alt="" />
+      {isHovered ? (
+        <video src={trailer} autoPlay={true} muted={true} loop />
+      ) : (
+        ""
+      )}
+
       <div className="item-info">
         <div className="icons">
-          <PlayCircleFilledWhiteOutlinedIcon />
-          <AddCircleOutlineOutlinedIcon />
-          <ThumbUpOutlinedIcon />
-          <ThumbDownOutlinedIcon />
+          <PlayCircleFilledWhiteOutlinedIcon className="icon" />
+          <AddCircleOutlineOutlinedIcon className="icon" />
+          <ThumbUpOutlinedIcon className="icon" />
+          <ThumbDownOutlinedIcon className="icon" />
         </div>
         <div className="itemInfoTop">
           <span>1 hour 20 minutes</span>
           <span className="limit">+16</span>
-          <div className="desc">
-            The film tells the story of Cassie (Carson, 29) and Marine Luke
-            (Galitzine, 27), who enter a "marriage of convenience" — with
-            unexpected results. Cassie, who is living with type I diabetes,
-            marries Luke, a Marine, as a way to get access to Luke's health
-            benefits.
-          </div>
-          <div className="genre">Romance</div>
+          <span>1999</span>
         </div>
+        <div className="desc">
+          The film tells the story of Cassie and Marine Luke.Cassie, who is
+          living with type I diabetes, marries Luke, a Marine, as a way to get
+          access to Luke's health benefits.
+        </div>
+        <div className="genre">Romance</div>
       </div>
     </div>
   );
