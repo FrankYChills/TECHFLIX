a53d1b0ab72ecf062c6477898bb19095952e1ced
franky <itskchills11@gmail.com>
2023-02-07 13:16:06 +0530

    added loader for single movie page

--- src/pages/movie/Movie.jsx
+++ src/pages/movie/Movie.jsx
@@ -4,6 +4,7 @@ import { useContext, useState } from "react";
 import { useEffect } from "react";
 import { Link, useParams } from "react-router-dom";
 import ArrowBackOutlinedIcon from "@mui/icons-material/ArrowBackOutlined";
+import HashLoader from "react-spinners/HashLoader";
 
 import AddCircleOutlineOutlinedIcon from "@mui/icons-material/AddCircleOutlineOutlined";
 import ThumbUpOutlinedIcon from "@mui/icons-material/ThumbUpOutlined";
@@ -34,27 +35,35 @@ const Movie = () => {
   }, []);
   return (
     <div className="movie">
-      <img src={movie.mainImg} className="bgImage" />
-      <div className="info">
-        <Link to="/" className="link">
-          <div className="back">
-            <ArrowBackOutlinedIcon className="icon" /> Home
+      {movie ? (
+        <>
+          <img src={movie.mainImg} className="bgImage" />
+          <div className="info">
+            <Link to="/" className="link">
+              <div className="back">
+                <ArrowBackOutlinedIcon className="icon" /> Home
+              </div>
+            </Link>
+            <img className="titleImg" src={movie.titleImg} />
+            <span className="desc">{movie.desc}</span>
+            <Link to={`/watch/${id}`} className="link">
+              <button className="play">
+                <PlayCircleFilledWhiteOutlinedIcon className="playicon" />
+                <span>Play</span>
+              </button>
+            </Link>
+            <div className="icons">
+              <AddCircleOutlineOutlinedIcon className="icon" />
+              <ThumbUpOutlinedIcon className="icon" />
+              <ThumbDownOutlinedIcon className="icon" />
+            </div>
           </div>
-        </Link>
-        <img className="titleImg" src={movie.titleImg} />
-        <span className="desc">{movie.desc}</span>
-        <Link to={`/watch/${id}`} className="link">
-          <button className="play">
-            <PlayCircleFilledWhiteOutlinedIcon className="playicon" />
-            <span>Play</span>
-          </button>
-        </Link>
-        <div className="icons">
-          <AddCircleOutlineOutlinedIcon className="icon" />
-          <ThumbUpOutlinedIcon className="icon" />
-          <ThumbDownOutlinedIcon className="icon" />
+        </>
+      ) : (
+        <div className="loader">
+          <HashLoader color="#36d7b7" size={100} />
         </div>
-      </div>
+      )}
     </div>
   );
 };
